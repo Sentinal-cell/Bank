d0091d95af4b898d98d7c142e7393d9a2323c2e8
Sentinal-cell <ahmadajiya12@gmail.com>
2024-06-21 16:33:33 -0600

    linode

--- src/ftran.java
+++ src/ftran.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.concurrent.TimeUnit;
-
+//hhjj
 public class ftran implements Runnable {
     private static int sbal;
     private static int rbal;
